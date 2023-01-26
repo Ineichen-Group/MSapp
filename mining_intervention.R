344081d78a6054178d68bb33cddee380dfe58c31
marianna-rosso-uzh <121856767+marianna-rosso-uzh@users.noreply.github.com>
2023-01-26 09:48:25 +0100

    Update mining_intervention.R

--- mining_intervention.R
+++ mining_intervention.R
@@ -1,3 +1,18 @@
+library(shiny)
+library(ggplot2)
+library(xlsx)
+library(readxl)
+library(dplyr)
+library(shinyWidgets)
+library(shinythemes)
+library(ggpubr)
+library(see)
+library(DT)
+library(shinysky)
+library(labelled)
+library(ggrepel)
+library(lubridate)
+
 # uploading dictionary manually curated
 
 dictionary<-read_excel("Druglist.xlsx")
