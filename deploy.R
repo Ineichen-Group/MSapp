f73670c9ddfb5c0a307cbe6a619d995035cd0d40
marianna-rosso-uzh <121856767+marianna-rosso-uzh@users.noreply.github.com>
2023-01-26 14:03:28 +0100

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,7 +1,6 @@
 # Authenticate
-setAccountInfo(name = Sys.getenv("ineichengroup"),
-               token = Sys.getenv("B919EE93E93687AD1216490ED3861589"),
-               secret = Sys.getenv("Na8tUFpfzjL7ILta02TuQGUGaa9Z+nrHL7jUXLJk"))
+setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+               token = Sys.getenv("TOKEN"),
+               secret = Sys.getenv("SECRET+nrHL7jUXLJk"))
 # Deploy
 deployApp(appFiles = c("app_MultipleSclerosis.R","complete_df_NCT_drugs.xlsx"))
-
