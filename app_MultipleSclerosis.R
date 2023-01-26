617b87cf20ef63f5210521e14d32a19317bfea6f
marianna-rosso-uzh <121856767+marianna-rosso-uzh@users.noreply.github.com>
2023-01-26 12:58:37 +0100

    Update app_MultipleSclerosis.R

--- app_MultipleSclerosis.R
+++ app_MultipleSclerosis.R
@@ -2,47 +2,105 @@ library(shiny)
 library(ggplot2)
 library(xlsx)
 library(readxl)
-library(tidyverse)
 library(dplyr)
 library(shinyWidgets)
 library(shinythemes)
 library(ggpubr)
 library(see)
-library(kableExtra)
 library(DT)
-library(shinysky)
+library(stringr)
 library(labelled)
+library(tidyr)
 library(ggrepel)
-library(shinymanager)
 library(lubridate)
+library(curl)
+library(data.table)
 
-# inactivity <- "function idleTimer() {
-# var t = setTimeout(logout, 120000);
-# window.onmousemove = resetTimer; // catches mouse movements
-# window.onmousedown = resetTimer; // catches mouse movements
-# window.onclick = resetTimer;     // catches mouse clicks
-# window.onscroll = resetTimer;    // catches scrolling
-# window.onkeypress = resetTimer;  //catches keyboard actions
-# 
-# function logout() {
-# window.close();  //close the window
-# }
-# 
-# function resetTimer() {
-# clearTimeout(t);
-# t = setTimeout(logout, 120000);  // time is in milliseconds (1000 is 1 second)
-# }
-# }
-# idleTimer();"
-# 
-# credentials <- data.frame(
-#   user = c("uzh"),
-#   password = c("uzh"),
-#   # comment = c("alsace", "auvergne", "bretagne"), %>% 
-#   stringsAsFactors = FALSE
-# )
+#update the data
+#uploading the cleaned new data
+df_clean<-read_excel(df_clean,"trials_clean.xlsx")
 
-complete_df_NCT_drugs <- complete_df_NCT_drugs %>% mutate(date=Sys.time())
+#uploading our existing table
+complete_df_NCT_drugs <- read_excel("complete_df_NCT_drugs.xlsx")
+
+# retreiving new records (i.e. new NCTIds) from the newly downloaded (and cleaned) data frame
+anti_match_NCTs<-
+  anti_join(df_clean %>% select(NCTId),complete_df_NCT_drugs%>% select(NCTId)) %>% unique() %>% 
+  merge(.,empty_df,by="NCTId")
+
+# uploading dictionary manually curated
+
+dictionary<-read_excel("Druglist.xlsx") %>% 
+  rename(Of_Drug_Name=Name)
+
+# cleaning the dictionary
+
+dictionary_pivot<-
+  dictionary %>%    
+  separate(Intervention,into=paste0("pivinterv_synonim",1:(max(data.frame(count=str_count(dictionary$Intervention,";")) %>% drop_na() %>% pull())+1)),sep=";") %>% 
+  pivot_longer(starts_with("pivinterv_synonim"),names_to="delete",values_to="synonims") %>% 
+  select(!(delete)) %>% 
+  drop_na(synonims)%>% 
+  mutate(synonims=str_trim(synonims, side = c("both"))) %>% 
+  mutate(lower_synonims=tolower(synonims))%>% 
+  mutate(lower_synonims=gsub("-"," ",lower_synonims)) %>% 
+  filter(!(lower_synonims==""|is.na(lower_synonims)))
+
+# subsetting the dictionary to allow smooth screening
+
+list<-split(dictionary_pivot, (seq(nrow(dictionary_pivot))) %/% 200)
+names(list) <- 0:max(as.numeric(names(list)))+1
+
+# loop preparation
+
+empT<-data.frame()  
+index_patter<-max(as.numeric(names(list)))
+
+#mining for the intervention in InterventionName and Brief title
+
+for(i in 1:index_patter){
+  data<-list[[i]]$lower_synonims
+  pattern<-paste0(data, collapse = "|")
+  df<-anti_match_NCTs %>% 
+    mutate(lower_intervention=tolower(InterventionName),lower_title=tolower(BriefTitle))%>%
+    mutate(lower_intervention=gsub("-"," ",lower_intervention),lower_title=gsub("-"," ",lower_title)) %>%
+    mutate(match_detect_intervention=str_detect(lower_intervention, pattern)) %>% 
+    mutate(match_detect_BT=str_detect(lower_title, pattern)) %>% 
+    mutate(replace=case_when(
+      match_detect_intervention==T&match_detect_BT==T~regmatches(lower_intervention, gregexpr(pattern, lower_intervention)),
+      match_detect_intervention==T&match_detect_BT==F~regmatches(lower_intervention, gregexpr(pattern, lower_intervention)),
+      match_detect_intervention==F&match_detect_BT==T~regmatches(lower_title, gregexpr(pattern, lower_title)))) %>% 
+    unnest_longer(replace)
+  empT<-rbind(empT,df) %>% unique
+}
+
+#append additional information to the original table 
+complete_df_NCT_drugs<-merge(complete_df_NCT_drugs,dictionary,by="Of_Drug_Name")
+
+#new table to append to the data set 
+
+mined_table<-empT %>% 
+  filter(!(is.na(replace))) %>% 
+  unique() %>% 
+  merge(.,dictionary_pivot,by.x="replace",by.y="lower_synonims",all.x=T)%>% 
+  mutate(URL=paste0("https://clinicaltrials.gov/ct2/show/",NCTId,"?term=",NCTId,"&draw=2&rank=1"))
+
+# appending to the original dataset
+
+bind_rows(complete_df_NCT_drugs, mined_table) %>% 
+  mutate(date=Sys.Date()) %>% 
+  unique() %>% 
+  write.xlsx("complete_df_NCT_drugs.xlsx")
+
+# table for unmined data
+
+unmined_table<- empT %>% 
+  filter(is.na(replace)) %>% 
+  write.xlsx("unmined_interventions.xlsx") 
+
+###################
+
+complete_df_NCT_drugs <- read_excel("complete_df_NCT_drugs.xlsx") %>% mutate(date=Sys.time())
 complete_df_NCT_drugs$Gender<-as.factor(complete_df_NCT_drugs$Gender)
 complete_df_NCT_drugs$StdAge<-as.factor(complete_df_NCT_drugs$StdAge)
 complete_df_NCT_drugs$Phase<-as.factor(complete_df_NCT_drugs$Phase)
@@ -77,8 +135,6 @@ data<-complete_df_NCT_drugs %>%
 
 
 ui_MS <- 
-  # secure_app(
-    # head_auth = tags$script(inactivity),
                     fluidPage(
   theme = shinytheme("yeti"),
   titlePanel(title=h2("Multiple Sclerosis treatments")),
@@ -127,12 +183,7 @@ ui_MS <-
 
 server_MS <- function(input, output) {
 
-  # result_auth <- secure_server(check_credentials = check_credentials(credentials))
-# 
-#   output$res_auth <- renderPrint({
-#     reactiveValuesToList(result_auth)
-#   })
-  
+
   data.d<-reactive({data[grepl(paste(input$search_drug,collapse="|"),data$`Treatment Name`),]})
   data.g<-reactive({data.d()[grepl(paste(input$Sex,collapse="|"),data.d()$Sex),]})
   data.p<-reactive({data.g()[grepl(paste(input$phase,collapse="|"),data.g()$Phase),]})
